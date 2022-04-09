fde43aec2e0c63d1bde39defa8b45546679c467b
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-04-09 16:08:43 +0200

    Remove unused files

--- src/containers/IndexPage/IndexPage.tsx
+++ src/containers/IndexPage/IndexPage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 
 import { Head } from 'seo/Head/Head';
-import { Visualiser } from 'sections/Visualiser/Visualiser';
 
 import * as S from './IndexPage.styles';
 
@@ -11,11 +10,10 @@ export default function IndexPage() {
     <>
       <Head />
       <S.Wrapper>
-        <Visualiser />
+        <Link href="/projects/meta-office" passHref>
+          <a style={{ fontSize: '14px' }}>Go to test</a>
+        </Link>
       </S.Wrapper>
-      <Link href="/projects/meta-office" passHref>
-        <a style={{ fontSize: '14px' }}>Go to test</a>
-      </Link>
     </>
   );
 }
--- src/sections/Visualiser/Visualiser.state.ts
+++
@@ -1,9 +0,0 @@
-import { App } from './classes/App';
-
-interface AppState {
-  app: App | null;
-}
-
-export const appState: AppState = {
-  app: null,
-};
