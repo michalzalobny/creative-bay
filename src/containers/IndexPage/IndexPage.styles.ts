8a3c7e2458a038e0f2bf14ef07352eb993c9df19
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-04-16 18:54:51 +0200

    Update index page

--- src/containers/IndexPage/IndexPage.styles.ts
+++ src/containers/IndexPage/IndexPage.styles.ts
@@ -1,3 +1,10 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+`;
