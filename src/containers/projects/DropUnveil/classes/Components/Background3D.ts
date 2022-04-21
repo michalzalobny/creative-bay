d01a83451b55ddd431de48d8fea8b112ae525099
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-04-22 00:33:49 +0200

    Update background colors

--- src/containers/projects/DropUnveil/classes/Components/Background3D.ts
+++ src/containers/projects/DropUnveil/classes/Components/Background3D.ts
@@ -17,8 +17,8 @@ export class Background3D extends InteractiveObject3D {
   _material: THREE.ShaderMaterial | null = null;
   _gui: GUI;
   _background = {
-    color1: [254 / 255, 178 / 255, 72 / 255],
-    color2: [0 / 255, 255 / 255, 213 / 255],
+    color1: [255 / 255, 120 / 255, 31 / 255],
+    color2: [0 / 255, 200 / 255, 255 / 255],
     colorAccent: [0 / 255, 0 / 255, 0 / 255],
     uLinesBlur: 0.25,
     uNoise: 0.075,
