87e2cae8636d29841b9ca9bbb7342e718ef2c3e1
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-04-24 08:56:43 +0200

    Update hue

--- src/containers/projects/DropUnveil/classes/shaders/text/fragmentCn.glsl
+++ src/containers/projects/DropUnveil/classes/shaders/text/fragmentCn.glsl
@@ -9,6 +9,13 @@ varying vec2 vUv;
 
 #define S(a,t) smoothstep(a*0.975, a, t)
 
+
+vec3 hueShift(vec3 color, float hue) {
+  const vec3 k = vec3(0.57735, 0.57735, 0.57735);
+  float cosAngle = cos(hue);
+  return vec3(color * cosAngle + cross(k, color) * sin(hue) + k * dot(k, color) * (1.0 - cosAngle));
+}
+
 void main() {
 
   vec2 mouse2D;
@@ -20,7 +27,7 @@ void main() {
   float radius = 0.5 * 250.0 / uPlaneRes.y;
   float dist = distance(mouse2D * aspect, vUv * aspect);
 
-  float refractionOffset = 0.035;
+  float refractionOffset = 0.036;
   float refractionPower = 0.007;
 
   float d1 = S(radius, dist);
@@ -30,13 +37,15 @@ void main() {
   sub *= aspect;
 
   vec2 uv = vUv - sub * pow(dist * 0.7, 0.7) + d2 * refractionPower;
-  vec4 tex_r = texture2D(tMap, uv);
-  vec4 tex_g = texture2D(tMap, uv + sub * 0.03);
-  vec4 tex_b = texture2D(tMap, uv + sub * 0.01);
+  vec4 tex_r = texture2D(tMap, uv - sub * 0.03);
+  vec4 tex_g = texture2D(tMap, uv + sub * 0.02);
+  vec4 tex_b = texture2D(tMap, uv + sub * 0.02);
   float a = max(max(tex_r.a, tex_g.a), tex_b.a);
   vec4 tex = vec4(tex_r.r, tex_g.g, tex_b.b, a);
 
   tex.a = mix(tex.a, 0.0, d1);
+
+  tex.rgb = hueShift(tex.rgb, 3.292);
 
   gl_FragColor = tex;
 }
