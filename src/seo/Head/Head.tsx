d3bc97b71651781a99a45df4d4c2a579a29ae588
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-05-05 22:31:17 +0200

    Update title

--- src/seo/Head/Head.tsx
+++ src/seo/Head/Head.tsx
@@ -12,13 +12,13 @@ export interface HeadProps {
 export const Head = (props: HeadProps) => {
   const {
     ogImage = 'https://res.cloudinary.com/dpv0ukspz/image/upload/v1650138958/og-100_mvbgru.jpg',
-    title = 'Creative Bay',
+    title = 'Portfolio 2022',
     description = 'Michal Zalobny portfolio 2022 🌬 WebGL & GLSL',
   } = props;
 
   return (
     <NextHead>
-      <title>{title}</title>
+      <title>{`Michal Zalobny - ${title}`}</title>
       <meta name="description" content={description} />
       <link
         rel="icon"
@@ -26,12 +26,12 @@ export const Head = (props: HeadProps) => {
       />
 
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={title} />
+      <meta property="og:title" content={`Michal Zalobny - ${title}`} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={ogImage} />
 
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
+      <meta name="twitter:title" content={`Michal Zalobny - ${title}`} />
       <meta name="twitter:description" content={description} />
 
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
