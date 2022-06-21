9c4a7a56bc3dbfd25e34ebca6150717cbfe0cce9
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-06-21 23:59:42 +0200

    Update preloader

--- src/utils/helperClasses/Preloader.ts
+++ src/utils/helperClasses/Preloader.ts
@@ -3,13 +3,17 @@ import * as THREE from 'three';
 import { GLTFLoader, GLTF, DRACOLoader } from 'three-stdlib';
 
 import { disposeModel } from 'utils/functions/disposeModel';
-import { LoadedAssets, AssetToPreload, AssetType } from 'utils/sharedTypes';
+import { LoadedAssets, AssetToPreload, AssetType, LoadedAsset } from 'utils/sharedTypes';
+
+interface AssignAsset extends LoadedAsset {
+  objPropertyName: string;
+}
 
 export class Preloader extends EventDispatcher {
   _assetsLoadedCounter = 0;
   _dracoLoader = new DRACOLoader();
   _gltfLoader = new GLTFLoader();
-  assetsToPreload: AssetToPreload[] = [];
+  _assetsToPreload: AssetToPreload[] = [];
   loadedAssets: LoadedAssets = {};
 
   constructor() {
@@ -18,81 +22,130 @@ export class Preloader extends EventDispatcher {
     this._gltfLoader.setDRACOLoader(this._dracoLoader);
   }
 
+  _assignAsset(props: AssignAsset) {
+    const { asset, naturalHeight, naturalWidth, objPropertyName, type } = props;
+    this.loadedAssets[objPropertyName] = {
+      type,
+      asset,
+      naturalWidth,
+      naturalHeight,
+    };
+    this._onAssetLoaded();
+  }
+
   _preloadTextures() {
-    if (this.assetsToPreload.length === 0) {
+    if (this._assetsToPreload.length === 0) {
       return this._onLoadingComplete();
     }
-    this.assetsToPreload.forEach(item => {
+
+    const handleImageLoad = (item: AssetToPreload) => {
+      const texture = new THREE.Texture();
+      const image = new window.Image();
+      image.crossOrigin = 'anonymous';
+      image.src = item.src;
+
+      const handleLoaded = () => {
+        texture.image = image;
+        texture.needsUpdate = true;
+        this._assignAsset({
+          objPropertyName: item.targetName || item.src,
+          type: AssetType.IMAGE,
+          asset: texture,
+          naturalWidth: image.naturalWidth,
+          naturalHeight: image.naturalHeight,
+        });
+      };
+      if (image.complete) {
+        return handleLoaded();
+      }
+      image.onload = () => {
+        handleLoaded();
+      };
+      image.onerror = () => {
+        this._assignAsset({
+          objPropertyName: item.targetName || item.src,
+          type: AssetType.IMAGE,
+          asset: texture,
+          naturalWidth: 1,
+          naturalHeight: 1,
+        });
+        console.error(`Failed to load image at ${item.src}`);
+      };
+    };
+
+    const handleVideoLoad = (item: AssetToPreload) => {
+      const video = document.createElement('video');
+      video.crossOrigin = 'anonymous';
+      video.muted = true;
+      video.loop = true;
+      video.controls = true;
+      video.playsInline = true;
+      video.autoplay = true;
+      video.src = item.src;
+      void video.play();
+
+      video.oncanplay = () => {
+        const texture = new THREE.VideoTexture(video);
+        this._assignAsset({
+          objPropertyName: item.targetName || item.src,
+          type: AssetType.VIDEO,
+          asset: texture,
+          naturalWidth: video.videoWidth,
+          naturalHeight: video.videoHeight,
+        });
+      };
+      video.onerror = () => {
+        const texture = new THREE.VideoTexture(video);
+        this._assignAsset({
+          objPropertyName: item.targetName || item.src,
+          type: AssetType.VIDEO,
+          asset: texture,
+          naturalWidth: 1,
+          naturalHeight: 1,
+        });
+        console.error(`Failed to load video at ${item.src}`);
+      };
+    };
+
+    const handleModel3DLoad = (item: AssetToPreload) => {
+      this._gltfLoader.load(
+        item.src,
+        (gltf: GLTF) => {
+          this._assignAsset({
+            objPropertyName: item.targetName || item.src,
+            type: AssetType.MODEL3D,
+            asset: gltf,
+            naturalWidth: 1,
+            naturalHeight: 1,
+          });
+        },
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
+        progress => {},
+        error => {
+          this._assignAsset({
+            objPropertyName: item.targetName || item.src,
+            type: AssetType.MODEL3D,
+            asset: null,
+            naturalWidth: 1,
+            naturalHeight: 1,
+          });
+          console.error(`Failed to load 3D model at ${item.src} `, error);
+        }
+      );
+    };
+
+    this._assetsToPreload.forEach(item => {
       switch (item.type) {
         case AssetType.IMAGE: {
-          const texture = new THREE.Texture();
-          const image = new window.Image();
-          image.crossOrigin = 'anonymous';
-          image.src = item.src;
-
-          const onLoad = () => {
-            texture.image = image;
-            texture.needsUpdate = true;
-            this.loadedAssets[item.targetName || item.src] = {
-              type: AssetType.IMAGE,
-              asset: texture,
-              naturalWidth: image.naturalWidth,
-              naturalHeight: image.naturalHeight,
-            };
-            this._onAssetLoaded();
-          };
-
-          if (image.complete) {
-            onLoad();
-            break;
-          }
-
-          image.onload = onLoad;
+          handleImageLoad(item);
           break;
         }
         case AssetType.VIDEO: {
-          const video = document.createElement('video');
-          video.crossOrigin = 'anonymous';
-          video.muted = true;
-          video.loop = true;
-          video.controls = true;
-          video.playsInline = true;
-          video.autoplay = true;
-          video.src = item.src;
-          void video.play();
-
-          video.oncanplay = () => {
-            const texture = new THREE.VideoTexture(video);
-            this.loadedAssets[item.targetName || item.src] = {
-              type: AssetType.VIDEO,
-              asset: texture,
-              naturalWidth: video.videoWidth,
-              naturalHeight: video.videoHeight,
-            };
-            this._onAssetLoaded();
-          };
+          handleVideoLoad(item);
           break;
         }
         case AssetType.MODEL3D: {
-          this._gltfLoader.load(
-            item.src,
-            (gltf: GLTF) => {
-              this.loadedAssets[item.targetName || item.src] = {
-                type: AssetType.MODEL3D,
-                asset: gltf,
-                naturalWidth: 1,
-                naturalHeight: 1,
-              };
-
-              this._onAssetLoaded();
-            },
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
-            progress => {},
-            error => {
-              console.warn('3D model loading failed', error);
-              this._onAssetLoaded();
-            }
-          );
+          handleModel3DLoad(item);
           break;
         }
         default:
@@ -104,7 +157,7 @@ export class Preloader extends EventDispatcher {
   _onAssetLoaded() {
     this._assetsLoadedCounter += 1;
 
-    const loadRatio = this._assetsLoadedCounter / this.assetsToPreload.length;
+    const loadRatio = this._assetsLoadedCounter / this._assetsToPreload.length;
 
     this.dispatchEvent({ type: 'progress', progress: loadRatio });
 
@@ -118,7 +171,7 @@ export class Preloader extends EventDispatcher {
   }
 
   setAssetsToPreload(items: AssetToPreload[]) {
-    this.assetsToPreload = items;
+    this._assetsToPreload = items;
     this._preloadTextures();
   }
 
