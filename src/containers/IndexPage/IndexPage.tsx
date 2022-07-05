904afcaf2a321b0d31d33b0a5bc3022de77cf2b1
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-07-05 11:54:18 +0200

    Update links

--- src/containers/IndexPage/IndexPage.tsx
+++ src/containers/IndexPage/IndexPage.tsx
@@ -12,23 +12,28 @@ export default function IndexPage() {
       <S.Wrapper>
         <S.ProjectsWrapper>
           <S.ProjectContainer>
+            <Link href="/projects/meta-office" passHref>
+              <S.ProjectLink>1. Meta Office</S.ProjectLink>
+            </Link>
+          </S.ProjectContainer>
+          <S.ProjectContainer>
             <Link href="/projects/exploding-particles" passHref>
-              <S.ProjectLink>1. Exploding Particles</S.ProjectLink>
+              <S.ProjectLink>2. Exploding Particles</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <Link href="/projects/drop-unveil" passHref>
-              <S.ProjectLink>2. Drop Unveil</S.ProjectLink>
+              <S.ProjectLink>3. Drop Unveil</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <Link href="/projects/shader-blob" passHref>
-              <S.ProjectLink>3. Shader Blob</S.ProjectLink>
+              <S.ProjectLink>4. Shader Blob</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <S.ProjectLink target="_blank" href="https://michalzalobny.com/">
-              4. Portfolio 2021
+              5. Portfolio 2021
             </S.ProjectLink>
           </S.ProjectContainer>
         </S.ProjectsWrapper>
