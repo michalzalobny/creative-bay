b26f88e5130c834e86aebeffc5ee9b5ac98c3838
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-08-02 01:13:17 +0200

    Update projects list

--- src/containers/IndexPage/IndexPage.tsx
+++ src/containers/IndexPage/IndexPage.tsx
@@ -12,28 +12,33 @@ export default function IndexPage() {
       <S.Wrapper>
         <S.ProjectsWrapper>
           <S.ProjectContainer>
+            <Link href="https://react-just-parallax.michalzalobny.com/" passHref>
+              <S.ProjectLink target="_blank">1. React Just Parallax</S.ProjectLink>
+            </Link>
+          </S.ProjectContainer>
+          <S.ProjectContainer>
             <Link href="/projects/meta-office" passHref>
-              <S.ProjectLink>1. Meta Office</S.ProjectLink>
+              <S.ProjectLink>2. Meta Office</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <Link href="/projects/exploding-particles" passHref>
-              <S.ProjectLink>2. Exploding Particles</S.ProjectLink>
+              <S.ProjectLink>3. Exploding Particles</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <Link href="/projects/drop-unveil" passHref>
-              <S.ProjectLink>3. Drop Unveil</S.ProjectLink>
+              <S.ProjectLink>4. Drop Unveil</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <Link href="/projects/shader-blob" passHref>
-              <S.ProjectLink>4. Shader Blob</S.ProjectLink>
+              <S.ProjectLink>5. Shader Blob</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <S.ProjectLink target="_blank" href="https://michalzalobny.com/">
-              5. Portfolio 2021
+              6. Portfolio 2021
             </S.ProjectLink>
           </S.ProjectContainer>
         </S.ProjectsWrapper>
