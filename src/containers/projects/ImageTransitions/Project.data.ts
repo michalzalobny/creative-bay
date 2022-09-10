e2f3743865a7af31d4cf077bb6d9c41c87273dd0
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-09-10 20:37:56 +0200

    Update links and name

--- src/containers/projects/ImageTransitions/Project.data.ts
+++ src/containers/projects/ImageTransitions/Project.data.ts
@@ -21,7 +21,7 @@ export const dataArray = [
     imgDisplacementSrc: '',
     title: 'Tiled Transition',
     repoHref:
-      'https://github.com/michalzalobny/creative-bay/blob/main/src/containers/projects/ImageTransitions/transitionShaders/fragment0.glsl',
+      'https://github.com/michalzalobny/creative-bay/tree/main/src/containers/projects/ImageTransitions/transitionShaders/s0',
     elId: 0,
     fragmentShader: fragment0,
     vertexShader: vertex0,
@@ -32,9 +32,9 @@ export const dataArray = [
     img2Src:
       'https://res.cloudinary.com/dpv0ukspz/image/upload/v1662632243/mehmet-turgut-kirkgoz-1-6BbBXcEf0-unsplash_1_klrw1p.jpg',
     imgDisplacementSrc: '',
-    title: 'Tiled Transition',
+    title: 'Roll Transition',
     repoHref:
-      'https://github.com/michalzalobny/creative-bay/blob/main/src/containers/projects/ImageTransitions/transitionShaders/fragment0.glsl',
+      'https://github.com/michalzalobny/creative-bay/tree/main/src/containers/projects/ImageTransitions/transitionShaders/s1',
     elId: 1,
     fragmentShader: fragment1,
     vertexShader: vertex1,
@@ -50,7 +50,7 @@ export const imagesToPreload = dataArray
 export const getStaticProps: GetStaticProps = () => {
   const head: HeadProps = {
     ogImage: 'https://res.cloudinary.com/dpv0ukspz/image/upload/v1650138958/og-100_mvbgru.jpg',
-    title: 'Three Starter project',
+    title: 'GLSL Image Transitions',
     description: 'Michal Zalobny portfolio 2022 WebGL & GLSL',
   };
 
