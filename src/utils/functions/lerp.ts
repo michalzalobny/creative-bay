354b91c8992da685374dac39f38a9f0540b2f0fc
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-09-21 19:09:02 +0200

    Update lerp function

--- src/utils/functions/lerp.ts
+++ src/utils/functions/lerp.ts
@@ -1,3 +1,6 @@
-export const lerp = (p1: number, p2: number, t: number) => {
-  return p1 + (p2 - p1) * t;
-};
+// export const lerp = (p1: number, p2: number, t: number) => {
+//   return p1 + (p2 - p1) * t;
+// };
+
+//Apparently less error (more accurate - avoids rounding errors)
+export const lerp = (a: number, b: number, n: number) => (1 - n) * a + n * b;
