1848e5872c1e87e267dc4aa3ac28abda5e67f8f9
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-10-18 21:43:57 +0200

    Update use effect

--- src/containers/projects/FirstPerson/Project.tsx
+++ src/containers/projects/FirstPerson/Project.tsx
@@ -19,14 +19,14 @@ export default function Project(props: PageProps) {
     console.log('loaded useEffect');
     if (!rendererEl.current) return;
 
-    appState.app = new App({ rendererEl: rendererEl.current, setShouldReveal, setProgressValue });
+    // appState.app = new App({ rendererEl: rendererEl.current, setShouldReveal, setProgressValue });
 
-    return () => {
-      if (appState.app) {
-        appState.app.destroy();
-        appState.app = null;
-      }
-    };
+    // return () => {
+    //   if (appState.app) {
+    //     appState.app.destroy();
+    //     appState.app = null;
+    //   }
+    // };
   });
 
   return (
