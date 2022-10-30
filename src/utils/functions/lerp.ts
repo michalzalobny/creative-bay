dfb89931eab5cb4540fd0ff37e9c05dd3b3e3d9c
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-10-31 00:15:57 +0100

    Update lerp

--- src/utils/functions/lerp.ts
+++ src/utils/functions/lerp.ts
@@ -1,6 +1,23 @@
-// export const lerp = (p1: number, p2: number, t: number) => {
-//   return p1 + (p2 - p1) * t;
-// };
+export const lerp = (p1: number, p2: number, t: number) => {
+  return p1 + (p2 - p1) * t;
+};
 
 //Apparently less error (more accurate - avoids rounding errors)
-export const lerp = (a: number, b: number, n: number) => (1 - n) * a + n * b;
+// export const lerp = (a: number, b: number, n: number) => (1 - n) * a + n * b;
+
+//Use together
+// export const lerp = (v0: number, v1: number, t: number) => {
+//   return v0 * (1 - t) + v1 * t;
+// };
+
+// export const lerp_ti = (
+//   source: number,
+//   target: number,
+//   rate: number,
+//   frameDelta = 1 / 60,
+//   targetFps = 60
+// ) => {
+//   const relativeDelta = frameDelta / (1 / targetFps);
+//   const smoothing = 1 - rate;
+//   return lerp(source, target, 1 - Math.pow(smoothing, relativeDelta));
+// };
