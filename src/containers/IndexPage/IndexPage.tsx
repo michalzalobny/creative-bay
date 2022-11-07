bf93622ac9d1e3240f1c8399203f79a071473fb7
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-11-07 13:26:42 +0100

    Update homepage

--- src/containers/IndexPage/IndexPage.tsx
+++ src/containers/IndexPage/IndexPage.tsx
@@ -12,38 +12,43 @@ export default function IndexPage() {
       <S.Wrapper>
         <S.ProjectsWrapper>
           <S.ProjectContainer>
+            <Link href="/projects/first-person" passHref>
+              <S.ProjectLink>1. First Person Controls</S.ProjectLink>
+            </Link>
+          </S.ProjectContainer>
+          <S.ProjectContainer>
             <Link href="/projects/image-transitions" passHref>
-              <S.ProjectLink>1. GLSL Image Transitions</S.ProjectLink>
+              <S.ProjectLink>2. GLSL Image Transitions</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <Link href="https://react-just-parallax.michalzalobny.com/" passHref>
-              <S.ProjectLink target="_blank">2. React Just Parallax</S.ProjectLink>
+              <S.ProjectLink target="_blank">3. React Just Parallax</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <Link href="/projects/meta-office" passHref>
-              <S.ProjectLink>3. Meta Office</S.ProjectLink>
+              <S.ProjectLink>4. Meta Office</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <Link href="/projects/exploding-particles" passHref>
-              <S.ProjectLink>4. Exploding Particles</S.ProjectLink>
+              <S.ProjectLink>5. Exploding Particles</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <Link href="/projects/drop-unveil" passHref>
-              <S.ProjectLink>5. Drop Unveil</S.ProjectLink>
+              <S.ProjectLink>6. Drop Unveil</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <Link href="/projects/shader-blob" passHref>
-              <S.ProjectLink>6. Shader Blob</S.ProjectLink>
+              <S.ProjectLink>7. Shader Blob</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
           <S.ProjectContainer>
             <S.ProjectLink target="_blank" href="https://michalzalobny.com/">
-              7. Portfolio 2021
+              8. Portfolio 2021
             </S.ProjectLink>
           </S.ProjectContainer>
         </S.ProjectsWrapper>
